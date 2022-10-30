d38ecf202676ef450842e3af5e879b8e3d14b146
Vladislav Semykin <vladislav_semykin01@mail.ru>
2022-10-30 22:15:34 +0300

    TODO

---
+++ task1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+#define z1 0.14
+#define t1 0.53
+#define N 30
+
+double z_seq(const double z, const double t, const int count)
+{
+}
+
+double t_seq(const double z, const double t, const int count)
+{
+}
+
+int main()
+{
+    return 0;
+}
